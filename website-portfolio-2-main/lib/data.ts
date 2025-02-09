133c0e7498cd09e3095f9effd86afa3e4f00439d
Thomas Bale <tokbale@outlook.com>
2025-02-09 21:30:21 +0000

    added emojis

--- website-portfolio-2-main/lib/data.ts
+++ website-portfolio-2-main/lib/data.ts
@@ -154,7 +154,7 @@ export const projectsData = [
   {
     title: "Quantum cross-chain arbitrage",
     description:
-        "$5000 winning project for the ETH Oxford DeFi hackathon. Quantum-Enhanced Cross-Chain Arbitrage Bot (QXAB) uses Quantum Optimization (QAOA) and Flare’s blockchain protocols to execute fast, secure, and profitable cross-chain arbitrage trades via flash loans.",
+        "🏆Winners - ETH Oxford DeFi hackathon prize ($5000)🏆 Quantum-Enhanced Cross-Chain Arbitrage Bot (QXAB) uses Quantum Optimization (QAOA) and Flare’s blockchain protocols to execute fast, secure, and profitable cross-chain arbitrage trades via flash loans.",
     tags: ["Blockchain", "Quantum", "Vyper","QAOA"],
     imageUrl: MeDIImg,
     link: "https://github.com/TumCucTom/quantum-cross-chain-arbitrage"
