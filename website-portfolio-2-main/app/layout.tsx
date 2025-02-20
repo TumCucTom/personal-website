b21571ce6313eaa8d57395387f9dc44a0a208325
Thomas Bale <tokbale@outlook.com>
2025-02-20 20:18:52 +0000

    no purple

--- website-portfolio-2-main/app/layout.tsx
+++ website-portfolio-2-main/app/layout.tsx
@@ -24,11 +24,6 @@ export default function RootLayout({
       <html lang="en" className='!scroll-smooth'>
       <body
           className='${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-40 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90'>
-      <div className=' bg-[#9b4de1] absolute top[-6rem] -z-10 right-[11rem] h-[29.25rem]
-         w-[31.25rem] rounded-full blur-[9rem] sm:w-[68.75rem]'></div>
-      <div className=' bg-[#6a0dad] absolute top[-1rem] -z-10 left-[-35rem] h-[26.25rem]
-         w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem]
-         lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
 
       <Analytics/>
       <ThemeContextProvider>
